eea0a5ab155597acb5faa336f9fe64fc2806f0fe
drewhollis <77352161+drewhollis@users.noreply.github.com>
2021-09-22 16:00:44 -0500

    Add files via upload

---
+++ src/SlideData.js
@@ -0,0 +1,115 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "/images/Tower1.jpg",
+    title: "Macaroon Tower",
+    desc: "Perfect For Parties",
+    bg: "F1FAEE",
+    button: "Customize Yours",
+  },
+  {
+    id: 2,
+    img: "/images/VerticalBox.jpg",
+    title: "Mix and Match Dozen Box",
+    desc: "Mix and Match All Flavors!",
+    bg: "F1FAEE",
+    button: "Order Mix and Match Dozen",
+  },
+  {
+    id: 3,
+    img: "/images/VerticalStack.jpg",
+    title: "Seasonal Flavors",
+    desc: "See Jess's Limited Time Seasonal Flavors",
+    bg: "F1FAEE",
+    button: "View Seasonal Flavors",
+  },
+];
+
+export const featuredflavors = [
+  {
+    id: 1,
+    img: "/images/SquareGroup.jpg",
+    title: "Flavor Name",
+  },
+  {
+    id: 2,
+    img: "/images/SquareRed.jpg",
+    title: "Flavor Name",
+  },
+  {
+    id: 3,
+    img: "/images/SquareGroupWTray.jpg",
+    title: "Flavor Name",
+  },
+];
+
+export const flavors = [
+  {
+    id: 1,
+    img: "/images/SquareGroup.jpg",
+    title: "Flavor Name",
+  },
+  {
+    id: 2,
+    img: "/images/SquareRed.jpg",
+    title: "Flavor Name",
+  },
+  {
+    id: 3,
+    img: "/images/SquareGroupWTray.jpg",
+    title: "Flavor Name",
+  },
+  {
+    id: 4,
+    img: "/images/SquareGroupWTray.jpg",
+    title: "Flavor Name",
+  },
+  {
+    id: 5,
+    img: "/images/SquareGroupWTray.jpg",
+    title: "Flavor Name",
+  },
+  {
+    id: 6,
+    img: "/images/SquareGroupWTray.jpg",
+    title: "Flavor Name",
+  },
+  {
+    id: 7,
+    img: "/images/SquareGroupWTray.jpg",
+    title: "Flavor Name",
+  },
+  {
+    id: 8,
+    img: "/images/SquareGroupWTray.jpg",
+    title: "Flavor Name",
+  },
+  {
+    id: 9,
+    img: "/images/SquareGroupWTray.jpg",
+    title: "Flavor Name",
+  },
+];
+
+export const packages = [
+  {
+    id: 1,
+    img: "/images/VerticalBox.jpg",
+    title: "Mix and Match Dozen",
+  },
+  {
+    id: 2,
+    img: "/images/VerticalCloseUpBox.jpg",
+    title: "Pre Selected Dozen",
+  },
+  {
+    id: 3,
+    img: "/images/VerticalStack.jpg",
+    title: "Mix and Match Half-Dozen",
+  },
+  {
+    id: 4,
+    img: "/images/VerticalStack.jpg",
+    title: "Pre Selected Half-Dozen",
+  },
+];
